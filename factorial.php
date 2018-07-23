8bfbaa16a8c3e6c71d723eaf8c956bc4473f31b6
rbecker8 <rbecker8@cnm.edu>
2018-07-23 08:56:15 -0600

    Created factorial function.

--- factorial.php
+++ factorial.php
@@ -1,1 +1,18 @@
 <?php
+
+function factorial($n) {
+				if(($n < 0 || $n > 20)) {
+					throw(new \Exception("Negatives and integers over 21 are not allowed"));
+				}
+				return true;
+}
+			try {
+						$fact = 1;
+						for ($i = 1; $i<=$fact-1; $i++) {
+									$fact = $fact * $i;
+						}
+
+
+			} catch (\Exception $exception) {
+					echo 'Message: ' .$exception->getMessage();
+			}
